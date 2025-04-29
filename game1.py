c3ab101eb462d4a54cd338d36bc796a1cedc2886
Eunice Wamu <ngangaeunice062@gmail.com>
2025-04-29 12:24:12 +0300

    Madlibs game
    create your own story

---
+++ game1.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# User will fill blank spaces to create a story
+
+adjective1 = input('Enter an adjective (description): ')
+adjective2 = input('Enter an adjective (description): ')
+verb = input('Enter a verb (action) ending with ing : ')
+noun = input('Enter a noun (person, animal, place, or thing): ')
+adjective3 = input('Enter an adjective (description): ')
+
+print(f'I woke up early in the morning feeling {adjective1}')
+print(f'The weather was {adjective2}')
+print(f'I started {verb} as i took my {noun}')
+print(f'I was so {adjective3}')
+
